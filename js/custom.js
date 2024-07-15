fa3737f7b9e021ffda7b4f5ea121eebb359e266a
desipossa <c1jang@hanmail.net>
2024-07-15 16:22:10 +0900

    20240715

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,3 @@
-IndexMove();
 MainFullpage();
 menuBtnAction();
 profileSlide();
@@ -6,65 +5,59 @@ profileSlide();
 function MainFullpage() {
     let tt = false;
     const nav = document.querySelectorAll('.gnb li');
-    const portFolio = new fullpage('.content', {
-        anchors: ['intro', 'pt01', 'pt02', 'pt03', 'profile'],
-        onLeave: function (origin, destination, direction) {
-            let idx = destination.index;
-            nav.forEach(it => it.classList.remove('on'))
-            nav[idx].classList.add('on');
-        }
-    });
-}
 
-function IndexMove() {
-    const tl = gsap.timeline({ repeat: 0, repeatDelay: 2 });
+    const tl = gsap.timeline({ pause: true, });
+
+    let t = 1;
+
 
     tl
         .from('.intro h2 span:nth-child(4)', {
             x: -400,
             opacity: 0,
-            duration: 0.4,
+            duration: t * 0.4,
+            delay: t * 1
         })
         .from('.intro h2 span:nth-child(1)', {
             rotateX: 720,
             y: -100,
-            delay: 1,
+            delay: t * 1,
             opacity: 0,
-            duration: 1,
+            duration: t * 1,
             ease: 'bounce'
         })
         .from('.intro h2 span:nth-child(2)', {
             y: 100,
-            delay: 1,
+            delay: t * 1,
             opacity: 0,
-            duration: 0.5,
+            duration: t * 0.5,
             ease: 'bounce'
         })
         .from('.intro h2 span:nth-child(3)', {
             y: -100,
-            delay: 1,
+            delay: t * 1,
             opacity: 0,
-            duration: 0.5,
+            duration: t * 0.5,
             ease: 'bounce'
         })
         .from('.intro h2 span:nth-child(5)', {
             y: 100,
-            delay: 1,
+            delay: t * 1,
             opacity: 0,
-            duration: 0.5,
+            duration: t * 0.5,
             ease: 'bounce'
         })
         .from('.intro h2 span:nth-child(6)', {
             y: -100,
-            delay: 1,
+            delay: t * 1,
             opacity: 0,
-            duration: 0.5,
+            duration: t * 0.5,
             ease: 'bounce'
         })
         .to('.intro h2 span:nth-child(4)', {
             width: 300,
-            duration: 1,
-            delay: 1,
+            duration: t * 1,
+            delay: t * 1,
         })
         .from('.intro p', {
             opacity: 0
@@ -79,8 +72,23 @@ function IndexMove() {
         })
         .from('.intro .bar', {
             width: 0,
-            duration: 2
+            duration: t * 2
         })
+
+
+    const portFolio = new fullpage('.content', {
+        anchors: ['intro', 'pt01', 'pt02', 'pt03', 'profile'],
+        onLeave: function (origin, destination, direction) {
+            let idx = destination.index;
+            nav.forEach(it => it.classList.remove('on'))
+            nav[idx].classList.add('on');
+            if (idx !== 0) {
+                tl.kill()
+            } else {
+                tl.restart()
+            }
+        }
+    });
 }
 
 
